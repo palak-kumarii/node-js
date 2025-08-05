91e2146a02f175b78a79d03f64313854bd83b049
palak-kumarii <pkumari.1712005@gmail.com>
2025-08-05 15:59:03 +0530

    add

---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("fs")
+const http = require("http");
+const os = require("os");
+const { Transform } = require("stream")
+
+
+console.log(os.platform())
+console.log(os.type())
+console.log(os.release())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.totalmem())
+console.log(os.freemem())
+console.log(os.uptime())
+console.log(os.hostname())
+console.log(os.userInfo())
+
+// const server = http.createServer((req, res) => {
+//     res.end("hello world")
+// })
+
+// server.listen(3000, () => {
+
+//         console.log("server is running at port number 3000")
+//     })
+// fs.writeFileSync("example.txt", "hello world")
+// console.log("file is created successfully")
+
+
+// const data = fs.readFileSync("example.txt", "utf - 8 ")
+// console.log(data)
+
+
+// if (fs.existsSync("example.txt")) {
+//     fs.unlinkSync("example.txt")
+//     console.log("file is deleted successfully")
+
+// } else {
+//     console.log("file does not exist")
+// }
+
+
+const read = fs.createReadStream("input.txt", "utf-8")
+read.on('data', (chunk) => {
+    console.log("chunk of data is fetched", chunk)
+})
+read.on('end', () => {
+    console.log("all data is get here")
+})
+read.on('error', (err) => {
+    console.log("error is occured", err)
+})
+const write = fs.createWriteStream("output.txt")
+
+
+// read.pipe(write)
+
+// write.on('finish', () => {
+//     console.log("all data is write inside the output file")
+// })
+// write.on('error', (err) => {
+//     console.log("error", err)
+// })
+
+
+const upperCaseStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const upperCase = chunk.toString().toUpperCase();
+        callback(null, upperCase)
+    }
+
+})
+read.pipe(upperCaseStream).pipe(write)
+write.on('finish', () => {
+    console.log("transform is completed")
+
+})
+read.on('error', (err) => {
+    console.log("error is occured", err)
+})
